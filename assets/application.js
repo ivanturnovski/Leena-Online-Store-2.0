3e7480d8a8bf1c3832cb16829301ee2b003cabac
Ivan Turnovski <ivanturnovski@hotmail.com>
2021-04-03 16:36:40 +0200

    Blog Page

--- assets/application.js
+++ assets/application.js
@@ -1,8 +1,12 @@
 // Sort By Filer On Collections START
-var select = document.querySelector('#sort_by');
-select.addEventListener('change', function (e) {
-	var url = new URL(window.location.href);
-	url.searchParams.set('sort_by', e.currentTarget.value);
-	window.location = url.href;
-});
+
+if (document.getElementById('sort_by') != null) {
+	var select = document.querySelector('#sort_by');
+	select.addEventListener('change', function (e) {
+		var url = new URL(window.location.href);
+		url.searchParams.set('sort_by', e.currentTarget.value);
+		window.location = url.href;
+	});
+}
+
 // Sort By Filer On Collections END
