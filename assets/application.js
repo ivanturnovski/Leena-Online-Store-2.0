e6d1a286790e6def792ce9fef1533790af0dbf7f
Ivan Turnovski <ivanturnovski@hotmail.com>
2021-04-03 14:30:19 +0200

    Product Filters(Sorting)

--- assets/application.js
+++ assets/application.js
@@ -1,2 +1,8 @@
-// Put your application javascript here
-console.log('js test');
+// Sort By Filer On Collections START
+var select = document.querySelector('#sort_by');
+select.addEventListener('change', function (e) {
+	var url = new URL(window.location.href);
+	url.searchParams.set('sort_by', e.currentTarget.value);
+	window.location = url.href;
+});
+// Sort By Filer On Collections END
