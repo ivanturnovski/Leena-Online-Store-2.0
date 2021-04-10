3283a8008c31d10f6f25312047dab5850c967902
Ivan Turnovski <ivanturnovski@hotmail.com>
2021-04-10 22:25:18 +0200

    Desktop + Mobile Menu + Other Fixes

--- assets/application.js
+++ assets/application.js
@@ -11,6 +11,7 @@ if (document.getElementById('sort_by') != null) {
 
 // Sort By Filer On Collections END
 
+// Slick Slider Start
 $(document).ready(function () {
 	$('.product-slider-for').slick({
 		slidesToShow: 1,
@@ -21,6 +22,7 @@ $(document).ready(function () {
 	});
 	$('.product-slider-nav').slick({
 		autoplay: true,
+		autoplaySpeed: 3000,
 		slidesToShow: 3,
 		slidesToScroll: 1,
 		asNavFor: '.product-slider-for',
@@ -28,4 +30,32 @@ $(document).ready(function () {
 		centerMode: true,
 		focusOnSelect: true,
 	});
+});
+// Slick Slider End
+
+$(document).ready(function () {
+	console.log(viewportWidth);
+
+	$('body').click(function (event) {
+		console.log(event.target);
+	});
+
+	var vw = $(window).width();
+	var viewportWidth = vw + 17;
+
+	console.log(viewportWidth);
+
+	if (viewportWidth < 992) {
+		var element = $('.has-dropdown');
+		element.click(function (event) {
+			event.preventDefault();
+			console.log('prev def');
+		});
+
+		$('.nav-link').click(function () {
+			var $content = $(this).next('.dropdown-menu');
+			$content.slideToggle();
+			$(this).toggleClass('active');
+		});
+	}
 });
